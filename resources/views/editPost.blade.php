a6c110497da4240061b4ddde36c582a0d8a45a0b
Brandon kyle <dbrandonkyle@gmail.com>
2024-08-03 16:18:20 +0800

    edit post

---
+++ resources/views/editPost.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Edit Post</h1>
+        <form action="/edit-post/{{$post->id}}" method="post">
+        @csrf
+        @method('put')
+        <input name="postTitle" type="text" value="{{$post->postTitle}}">
+        <textarea name="postContent" type="text">{{$post->postContent}}</textarea>
+        <button>Save Changes</button>
+</body>
+</html>
